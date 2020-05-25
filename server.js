a4b1f0b2cbcb7f7613929210c23cf8d0122c1cf0
Abhishek Tandon <web.tandon@gmail.com>
2020-05-25 16:02:24 +0530

    server.js updated

--- server.js
+++ server.js
@@ -16,11 +16,9 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production ') {
+if (process.env.NODE_ENV === 'production') {
   // set static folder
-  app.use(
-    express.static(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
